a2fd2736e78b1ffff963b67f1c4eda89cd888b21
Gijs <gijs.nagtegaal@hva.nl>
2026-09-01 20:57:03 +0200

    Headings component completed [Component] Headings Fixes GijsNagtegaal/your-tribe-for-life-squad-page#9

---
+++ src/lib/components/Heading.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { level = 1, text = '' } = $props();
+</script>
+
+<svelte:element class="heading" this={`h${level}`}>
+    {text}
+</svelte:element>
+
+<style>
+    .heading {
+        margin: 1rem 0
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
     import Navbar from "$lib/components/Navbar.svelte";
+    import Heading from "$lib/components/Heading.svelte";
 </script>
 
 <Navbar />
+
+<Heading level={4} text="Groups" />
+
+<style>
+    
+</style>
