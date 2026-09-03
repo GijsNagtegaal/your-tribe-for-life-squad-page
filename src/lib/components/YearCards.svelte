955ffd37409c5de60dc5c36233fde675c7d67f8c
ThomasSeisveld <tseisveld@gmail.com>
2026-09-03 14:01:52 +0200

    #6 content first HTML

---
+++ src/lib/components/YearCards.svelte
@@ -0,0 +1,21 @@
+<script>
+    let {persons = [] } = $props();
+    // const persons = data.persons;
+</script>
+
+<section>
+{#each persons as persons}
+    <article>
+        <ul>
+            <li><p>{persons.name}</p></li>
+        </ul>
+    </article>
+{/each}
+
+    <article>
+        <figure>
+           
+        </figure>
+        <h3>jaar 1</h3>
+    </article>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,14 @@
     import Prevpage from "$lib/components/Prevpage.svelte";
     import Heading from "$lib/components/Heading.svelte";
     import GroupView from "$lib/components/GroupView.svelte";
+    import YearCards from "$lib/components/YearCards.svelte";
 </script>
 <main>
     <Prevpage url="/terug" text="terug" />
     <Navbar />
     <Heading level={4} text="Groups" />
     <GroupView />
+    <YearCards {persons} />
 </main>
 
 <Player />
