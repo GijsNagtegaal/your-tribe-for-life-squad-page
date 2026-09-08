cb5c073bc0c9df8d8e8ba0d1fe06e4156f70db50
Roxy <roxyfokker@hotmail.com>
2026-09-08 18:32:03 +0200

    feat: component used in teachers #7

--- src/lib/components/TeachersCaroussel.svelte
+++ src/lib/components/TeachersCaroussel.svelte
@@ -1,6 +1,7 @@
 <script>
     let { persons } = $props();
     import mugshotUnknown from '$lib/assets/mugshot-unknown.png';
+    import PersonSquare from './PersonSquare.svelte';
 
 
 </script>
@@ -10,11 +11,9 @@
         {#each persons as person}
             <li>
                 <a href="/docenten/{person.slug}">
-                    <figure style="--favcolor: {person.fav_color ?? 'var(--color-secondary-light)'};">
-                    <div class="image-wrapper">
-                        <img src={person.mugshot ? `https://fdnd.directus.app/assets/${person.mugshot}` : mugshotUnknown} alt="foto van {person.name}" style="--favborderradius: {person.fav_border_radius}px;">
-                    </div>
-                            <figcaption>{person.name}</figcaption>
+                    <figure>
+                        <PersonSquare {person} />
+                        <figcaption>{person.name}</figcaption>
                     </figure>
                 </a>
             </li>
@@ -39,22 +38,6 @@
         padding-left: 0;
         align-items: center;
         width: 10rem;
-
-        .image-wrapper{
-            background-color: var(--favcolor); /*word de fav color van die persoon */
-            padding: var(--spacing-sm);
-            aspect-ratio: 1;
-        }
-        
-        img{
-            border-radius: var(--border-radius-circle); /*word de fav border radius van die persoon */
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: center;
-            object-fit: cover;
-            border-radius: var(--favborderradius);
-        }
 
         figcaption{
             align-self: flex-start;
