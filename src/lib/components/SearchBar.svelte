1df6ef8686560c82a49ae29d390d760f7400d5ae
Roxy <roxyfokker@hotmail.com>
2026-09-08 14:02:52 +0200

    style: styling searchbar default en hover #38

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,46 @@
+<script>
+    import SearchIcon from "$lib/components/icons/SearchIcon.svelte";
+    let { persons } = $props();
+</script>
+
+<header>
+    <h2>Vind je klasgenoten en docenten.</h2>
+    <search>
+        <form>
+            <label for="">
+                <SearchIcon size="2rem" />
+                <input type="search" placeholder="Wie wil je opzoeken?">
+            </label>
+        </form>
+    </search>
+</header>
+
+<style>
+    form{
+        display: flex;
+        flex-direction: column;
+    }
+    label{
+        display: flex;
+		align-items: center;
+		gap: var(--spacing-sm);
+		padding: var(--spacing-md) var(--spacing-lg);
+		border: 0.15rem solid var(--color-neutral-darker);
+		border-radius: var(--border-radius-3xl);
+		background-color: var(--color-neutral-darker);
+
+        &:hover, &:focus-within{
+            border-color: var(--color-neutral-lighter);
+        }
+    }
+    input{
+        border: none;
+        background: none;
+        outline: none;
+        width: 100%;
+    }
+    input::placeholder{
+        font-size: var(--font-size-body-md);
+        font-family: var(--font-primary);
+    }
+</style>
---
+++ src/routes/zoeken/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import SearchBar from "$lib/components/SearchBar.svelte";
+
+    let { data } = $props();
+</script>
+
+<SearchBar persons={data.persons} />
+
+<style>
+
+</style>
