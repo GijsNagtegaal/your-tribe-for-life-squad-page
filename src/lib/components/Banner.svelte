9a240d517934027f4b8ab9bdf01d0ba8d740e454
Thomas Seisveld <tseisveld@gmail.com>
2026-09-09 14:00:54 +0200

    #35 add CSS styling

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -12,3 +12,46 @@
         <h4>Frontend design &amp; development</h4>
     </header>
 </article>
+
+<style>
+    article {
+        display: flex;
+        align-items: center;
+        height: 69px;
+        padding: 0 29px;
+
+        figure {
+            width: 51px;
+            height: 51px;
+            margin: 0;
+            flex-shrink: 0;
+
+            img {
+                display: block;
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                border-radius: var(--border-radius-xs);
+            }
+        }
+        header {
+            margin-left: 10px;
+
+             p {
+                margin: 0;
+                color: var(--color-neutral-dark);
+                font-size: var(--font-size-body-xs);
+                line-height: 12px;
+                font-weight: var(--font-weight-regular);
+            }
+
+            h4 {
+                margin: 0;
+                color: var(--text);
+                line-height: 25px;
+                font-weight: var(--font-weight-bold);
+                letter-spacing: -0.8px;
+            }
+        }
+    }
+</style>
