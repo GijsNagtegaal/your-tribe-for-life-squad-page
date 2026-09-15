9b1713887f3f970bcad4346573b4107ca360c0ec
Roxy <roxyfokker@hotmail.com>
2026-09-15 11:48:14 +0200

    style: studenten overzicht met gradients #37

--- src/routes/studenten/[year=year]/+page.svelte
+++ src/routes/studenten/[year=year]/+page.svelte
@@ -21,14 +21,19 @@
     {/each}
 </ul>
 
+
 <style>
     ul{
+        position: relative;
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(0, 200px));
+        /*https://css-tricks.com/books/greatest-css-tricks/flexible-grids/*/
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
         row-gap: var(--spacing-md);
         padding: 0;
         margin: 0 auto;
         max-width: 1000px;
+        padding: var(--spacing-md);
+
         li{
             display: flex;
             flex-direction: column;
@@ -38,6 +43,39 @@
                 padding: var(--spacing-md);
             }
         }
+
+        &::before{
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            width: 100%;
+            height: 60vh;
+            background: linear-gradient(
+                180deg,
+                #00667b 0%,
+                #002f38 49%,
+                #101010 89%
+            );
+        }
+        &::after{
+            content: "";
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 60vh;
+            z-index: 2;
+            pointer-events: none;
+            background: linear-gradient(
+                0deg,
+                rgba(0, 0, 0, 0.7) 0%,
+                rgba(0, 0, 0, 0.47) 49%,
+                rgba(0, 0, 0, 0) 89%
+            );
+        }
     }
+   
     
 </style>
