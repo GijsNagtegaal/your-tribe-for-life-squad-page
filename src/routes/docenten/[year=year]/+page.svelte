21bb9722daa39755e2d1854c2edb0f4f4cc1851c
Roxy <roxyfokker@hotmail.com>
2026-09-15 12:26:54 +0200

    feat: docenten overzicht #37

--- src/routes/docenten/[year=year]/+page.svelte
+++ src/routes/docenten/[year=year]/+page.svelte
@@ -1,1 +1,83 @@
-<p>hoi ik test dit ook ff</p>
+<script>
+    import Prevpage from '$lib/components/Prevpage.svelte';
+    import Dropdown from '$lib/components/Dropdown.svelte';
+    import ProfileImage from '$lib/components/ProfileImage.svelte'
+    import ProfileHeading from '$lib/components/ProfileHeading.svelte'
+
+    let { data } = $props();
+    const teachers = data.teachers;
+</script>
+
+
+<Prevpage />
+<Dropdown />
+
+<ul>
+    {#each teachers as teacher}
+        <li>
+            <a href="/studenten/{teacher.slug}">
+                <ProfileImage person={teacher} />
+                <h4>{teacher.name}</h4>
+            </a>
+        </li>
+    {/each}
+</ul>
+
+
+<style>
+    ul{
+        position: relative;
+        display: grid;
+        /*https://css-tricks.com/books/greatest-css-tricks/flexible-grids/*/
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        row-gap: var(--spacing-md);
+        padding: 0;
+        margin: 0 auto;
+        max-width: 1000px;
+        padding: var(--spacing-md);
+
+        li{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
+            h4{
+                padding: var(--spacing-md);
+            }
+        }
+
+        &::before{
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            width: 100%;
+            height: 60vh;
+            background: linear-gradient(
+                180deg,
+                #00667b 0%,
+                #002f38 49%,
+                #101010 89%
+            );
+        }
+        &::after{
+            content: "";
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 60vh;
+            z-index: 2;
+            pointer-events: none;
+            background: linear-gradient(
+                0deg,
+                rgba(0, 0, 0, 0.7) 0%,
+                rgba(0, 0, 0, 0.47) 49%,
+                rgba(0, 0, 0, 0) 89%
+            );
+        }
+    }
+   
+    
+</style>
