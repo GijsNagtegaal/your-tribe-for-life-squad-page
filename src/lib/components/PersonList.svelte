6b805ff981dbcbb5276feb88b592ed11a7e80399
Roxy <roxyfokker@hotmail.com>
2026-09-15 12:31:48 +0200

    feat: component maken van personlist #37

---
+++ src/lib/components/PersonList.svelte
@@ -0,0 +1,77 @@
+<script>
+    import ProfileImage from '$lib/components/ProfileImage.svelte'
+    import ProfileHeading from '$lib/components/ProfileHeading.svelte'
+
+    let { persons } = $props();
+</script>
+
+
+<ul>
+    {#each persons as person}
+        <li>
+            <a href="/studenten/{person.slug}">
+                <ProfileImage />
+                <h4>{person.name}</h4>
+            </a>
+        </li>
+    {/each}
+</ul>
+
+
+<style>
+    ul{
+        position: relative;
+        display: grid;
+        /*https://css-tricks.com/books/greatest-css-tricks/flexible-grids/*/
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        row-gap: var(--spacing-md);
+        padding: 0;
+        margin: 0 auto;
+        max-width: 1000px;
+        padding: var(--spacing-md);
+
+        li{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
+            h4{
+                padding: var(--spacing-md);
+            }
+        }
+
+        &::before{
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            width: 100%;
+            height: 60vh;
+            background: linear-gradient(
+                180deg,
+                #00667b 0%,
+                #002f38 49%,
+                #101010 89%
+            );
+        }
+        &::after{
+            content: "";
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 60vh;
+            z-index: 2;
+            pointer-events: none;
+            background: linear-gradient(
+                0deg,
+                rgba(0, 0, 0, 0.7) 0%,
+                rgba(0, 0, 0, 0.47) 49%,
+                rgba(0, 0, 0, 0) 89%
+            );
+        }
+    }
+   
+    
+</style>
